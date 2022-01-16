4f235775dcbf64f533b480ff1336baa3af6999de
Marcin Jurczak <marcin.jurczak12@gmail.com>
2022-01-16 13:36:32 +0100

    Initial commit

---
+++ bookmarks.js
@@ -0,0 +1,35 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+    {
+        title: "Daily",
+        links: [
+            { name: "Gmail", url: "https://inbox.google.com" },
+            { name: "GitHub", url: "https://github.com" },
+            { name: "Drive", url: "https://drive.google.com" },
+        ],
+    },
+    {
+        title: "Media",
+        links: [
+            { name: "Youtube", url: "https://youtube.com" },
+            { name: "Twitch", url: "https://twitch.tv" },
+            { name: "Netflix", url: "https://netflix.com" },
+        ],
+    },
+    {
+        title: "Social",
+        links: [
+            { name: "Facebook", url: "https://facebook.com" },
+            { name: "Twitter", url: "https://twitter.com" },
+            { name: "HLTV", url: "https://www.hltv.org/" },
+        ],
+    },
+    {
+        title: "School",
+        links: [
+            { name: "Excel", url: "https://docs.google.com/spreadsheets/d/1OeM4sS33T5rvDLUFloq16oKkjZhiHvsfQ-mJTYiQyFM/edit#gid=0"},
+            { name: "Moodle", url: "https://enauczanie.pg.edu.pl/moodle/my/" },
+            { name: "Ogarniacz™", url: "https://docs.google.com/spreadsheets/d/1OeM4sS33T5rvDLUFloq16oKkjZhiHvsfQ-mJTYiQyFM/edit#gid=0" },
+        ]
+    }
+];
